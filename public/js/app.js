e839529b8255701cd68c3f24abbbebc515c01aac
Eric W. Greene <eric@ericwgreene.com>
2017-11-28 14:17:43 -0500

    added declarations demo

--- public/js/app.js
+++ public/js/app.js
@@ -1,45 +1,8 @@
 
-let t;
 
-t = undefined;
-console.log(t);
-console.log(typeof t);
 
-t = 2;
-console.log(t);
-console.log(typeof t);
 
-t = "Hello, World!";
-console.log(t);
-console.log(typeof t);
 
-t = true;
-console.log(t);
-console.log(typeof t);
-
-t = Symbol('my first symbol');
-console.log(t);
-console.log(typeof t);
-
-t = null;
-console.log(t);
-console.log(typeof t);
-
-t = { id: 1 };
-console.log(t);
-console.log(typeof t);
-
-t = [1,2,3,4,5];
-console.log(t);
-console.log(typeof t);
-
-t = function() { };
-console.log(t);
-console.log(typeof t);
-
-t = class { };
-console.log(t);
-console.log(typeof t);
 
 
 
