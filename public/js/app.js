18533be8ac6bb1a80f7e1e950719fb36a820ec4b
Eric W. Greene <eric@ericwgreene.com>
2017-11-29 14:44:05 -0500

    added code demos

--- public/js/app.js
+++ public/js/app.js
@@ -27,18 +27,30 @@
 
 
 const youngMan = new Promise(function youngLady(resolve, reject) {
-
   setTimeout(function() {
-    resolve('yes, but my mom and dad are making me');
-    //reject('no, I have fallen in love with your best friend');
+    resolve('yes, he is the love of my life!');
   }, 2000);
-
 });
 
 youngMan.then(function(results) {
   console.log(results);
   console.log('yay! she said yes!');
-}).then(function)..catch(function(results) {
+  console.log('go find a place');
+  return new Promise(function findAPlace(resolve) {
+    setTimeout(function() {
+      resolve('found a nice place that was reasonably priced!');
+    }, 2000);
+  });
+}).then(function(results) {
+  console.log(results);
+  console.log('go find some wedding clothes');
+  return new Promise(function findSomeWeddingClothes(resolve) {
+    setTimeout(function() {
+      resolve('found wedding clothes that were reasonably priced!');
+    }, 2000);
+  });
+}).then(function(results) {
+}).catch(function(results) {
   console.log(results);
   console.log('he is going to sign up for eHarmony...');
 });
